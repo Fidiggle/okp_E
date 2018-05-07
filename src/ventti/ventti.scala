eb26c03aa2b873ab347e9904d6e2cf440e7dda6d
aegan123 <juhani@fmail.co.uk>
2018-05-07 15:43:04 +0300

    added hand lists

--- src/ventti/ventti.scala
+++ src/ventti/ventti.scala
@@ -1,5 +1,18 @@
 package ventti
+import collection.mutable._
+import Kortti
 
-class ventti {
+class Ventti {
+  val dealers_hand=new ListBuffer()
+  val players_hand=new ListBuffer()
+  val deck=new ListBuffer()
+  val valueOfHands=Array(0,0) 
+  
+  def isAce(kortti:Kortti){
+    kortti.getValue()==1
+  }
+  def isPicture(kortti:Kortti){
+    kortti.getValue()>10
+  }
   
 }
